48e7946e95ad0d09ca0855a52b2a576a8defa037
oliullah05 <mdolihasan@gmail.com>
2023-05-05 16:04:59 +0600

    register form and login with google and github done

--- src/pages/Account/Login.jsx
+++ src/pages/Account/Login.jsx
@@ -71,28 +71,37 @@ const Login = () => {
 
     return (
         <div className="py-6 lg:py-24">
-            <div className=" bg-white rounded-lg shadow-lg mx-auto max-w-sm lg:max-w-xl">
+            <div className=" bg-slate-100 rounded-lg shadow-lg mx-auto max-w-sm lg:max-w-xl">
                 <form onSubmit={handleSignIn} className="w-full p-8">
-                    <h2 className="text-2xl font-semibold text-gray-700 text-center">The Chef's Corner</h2>
-                    <p className="text-xl text-gray-600 text-center">Please Login!</p>
+            
+                    <p className="text-3xl text-blue-600 text-center">Please Login!</p>
                     <div className="mt-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">Email Address</label>
-                        <input className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="email" name="email" required />
+                        <label className="block font-bold mb-2">Email Address</label>
+                        {/* <input className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none"  /> */}
+
+
+                        <input type="email" name="email" placeholder="Type Your Email Here" className="input input-bordered input-primary w-full " required />
                     </div>
                     <div className="mt-4">
                         <div className="flex justify-between">
-                            <label className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-                            <p className="text-xs text-gray-500 cursor-pointer">Forget Password?</p>
+                            <label className="block  font-bold mb-2">Password</label>
+                            
                         </div>
-                        <input className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="password" name="password" required />
+                        {/* <input className="bg-gray-200 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" /> */}
+                        <input type="password" name="password"  placeholder="Type Your Passowrd Here" className="input input-bordered input-primary w-full" required/>
                     </div>
-                    <div className="mt-8">
-                        <button className="bg-gray-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-gray-600" type='submit'>Login</button>
+                    <div className="mt-8 flex justify-center">
+                        <button className="btn btn-active btn-primary w-64" type='submit'>Login</button>
                     </div>
                     <div className="mt-4 flex items-center justify-between">
-                        <span className="border-b w-1/5 md:w-1/4"></span>
+                        {/* <span className="border-b w-1/5 md:w-1/4"></span> */}
                         <p className="text-sm text-center text-gray-500">Don't have an account? <Link to={"/account/register"} className='underline'>Register</Link></p>
-                        <span className="border-b w-1/5 md:w-1/4"></span>
+                        {/* <span className="border-b w-1/5 md:w-1/4"></span> */}
+                        <Link className='underline'>forgot password?</Link>
+                    </div>
+
+                    <div className='mt-4'>
+
                     </div>
                     <div className='mt-4'>
                         {
@@ -107,11 +116,11 @@ const Login = () => {
                     <div className='flex flex-col lg:flex-row gap-3'>
                         <button onClick={handleGoogleSignIn} className="flex gap-2 items-center w-full justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100">
                             <FaGoogle className='text-black' />
-                            <span className="py-3 text-center text-gray-600 font-bold">Google</span>
+                            <span className="py-3 text-cente font-bold text-primary">Login With Google</span>
                         </button>
                         <button onClick={handleGithubSignIn} className="flex gap-2 items-center w-full justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100">
                             <FaGithub className='text-black' />
-                            <span className="py-3 text-center text-gray-600 font-bold">Github</span>
+                            <span className="py-3 text-center text-primary font-bold">Login With Github</span>
                         </button>
                     </div>
                 </form>
